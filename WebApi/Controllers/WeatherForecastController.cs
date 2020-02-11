360e4f275841daf93510db6264c132f06b8103e0
Joe <tamatenoworries@yahoo.com>
2020-02-12 02:09:39 +1030

    Add project files.

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var list = new List<WeatherForecast>();
+            var rng = new Random();
+            for(int i = 0; i < 5; i++)
+            {
+                WeatherForecast w = new WeatherForecast();
+                w.Date = DateTime.Now.AddDays(i);
+                w.Summary = Summaries[rng.Next(Summaries.Length)];
+                w.TemperatureC=rng.Next(-20, 55);
+                list.Add(w);
+            }
+            return list;
+
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+        }
+    }
+}
---
+++ WebApi/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class User
+    {
+        public String name { get; set; }
+        public String id { get; set; }
+    }
+}
